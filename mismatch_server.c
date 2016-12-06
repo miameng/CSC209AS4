743decf77c3db0b7fc33c22da24fb9c94049aa53
Mia Meng <mengxiangying1@gmail.com>
2016-12-06 18:00:07 -0500

    version1

---
+++ mismatch_server.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/signal.h>
+
+
+// defining a struct for client
+//struct client *head = NULL;
+struct client {
+    int fd;
+    struct in_addr ipaddr;
+    char username[128];
+    struct client *next;
+} *head = NULL;
+
+void addclient(int fd, struct in_addr add);
+void removeclient(int fd);
+
+
+int main(int argc, char **argv)
+{
+	// argument check
+
+
+	// defining a socket
+	int serv_socket_fd;
+	int port = 56288;
+	if((serv_socket_fd = socket(AF_INET, SOCK_STREAM,0)) < 0){
+		perror("socket");
+		exit(1);
+	}
+
+	// asigning an address for the socket
+	struct sockaddr_in server_addr; // declaring variable 'server_addr' of type socketaddr_un
+	memset(&server_addr,'\0',sizeof(server_addr)); // clear all bytes to zero
+	server_addr.sin_family = AF_INET; // set up address family NOTE: change to sin_family if this doesn't work
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+
+	// bind
+	//struct sockaddr_un serv_addr; // ------------is it the same as line 32????
+	if(bind(serv_socket_fd,(struct socketadd*)&server_addr,sizeof(server_addr))){
+		perror("bind");
+		return(1);
+	}
+
+	// listen
+	if (listen(serv_socket_fd, 5)){
+		perror("listen");
+		return(1);
+	}
+
+	// accept
+	while(1){
+		struct sockaddr_in client_addr;
+		int len = sizeof (client_addr); 
+		int client_fd;
+		if ((client_fd = accept(serv_socket_fd, (struct sockaddr *)&client_addr, &len)) < 0) {
+			perror("accept");
+			return(1);
+		}
+		printf("connection from %s\n", inet_ntoa(client_addr.sin_addr));
+		addclient(client_fd, client_addr.sin_addr);
+		int tttt;
+		while (1) {
+		    fd_set fdlist;
+		    struct timeval tv;
+		    FD_ZERO(&fdlist);
+		    FD_SET(client_fd, &fdlist);
+		    tv.tv_sec = 15;
+		    tv.tv_usec = 0;
+		    switch (select(client_fd+1, &fdlist, NULL, NULL, &tv)){
+				case 0:
+					printf("abcd\n");
+					removeclient(client_fd);
+					break;
+				case -1:
+					perror("select\n");
+					break;
+				default:
+					printf("hahaha\n");
+		    }
+		    
+		}
+
+	}
+	return(0);
+}
+
+void addclient(int fd, struct in_addr add){
+	struct client *p = malloc(sizeof(struct client));
+	if(!p){
+		fprintf(stderr, "fail to allocate memory\n");
+		exit(1);
+	}
+	p->fd = fd;
+	p->ipaddr = add;
+
+	p->next = head;
+	head = p;
+
+	printf("Adding client %s\n", inet_ntoa(add));
+}
+
+
+void removeclient(int fd){
+	struct client *p = &head;
+
+	// -----------QQQ try to find a way to reduce the running time
+	while(p != NULL){
+		p = p->next;
+		if (p->fd == fd){ 
+			struct client *temp;
+			temp = p->next;
+			free(p);
+			p = temp;
+		}
+	}
+	printf("Removing client #######");
+
+}
+
+
+
+
+
