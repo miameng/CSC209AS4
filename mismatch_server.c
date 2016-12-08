9fe708badceeaf570ceefa590013187d995098ee
Mia Meng <mengxiangying1@gmail.com>
2016-12-07 21:26:32 -0500

    multi-client

--- mismatch_server.c
+++ mismatch_server.c
@@ -11,7 +11,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/signal.h>
-
+#include "utils.h"
 
 // -------QQQQQ: without defining the port, I can still connect it-------QQQQQQ
 // --- define the port that the server looking forward to listen to
@@ -19,14 +19,8 @@
   #define PORT 56288
 #endif
 
-// defining a struct for client
-//struct client *head = NULL;
-struct client {
-    int fd;
-    struct in_addr ipaddr;
-    char username[128];
-    struct client *next;
-} *head = NULL;
+Client *head = NULL;
+char *askname = "What is your user name?\n";
 
 void addclient(int fd, struct in_addr add);
 void removeclient(int fd);
@@ -34,12 +28,17 @@ void removeclient(int fd);
 
 int main(int argc, char **argv)
 {
-	// argument check
+	Client *curr;
+	extern void newconnection(int serv_socket_fd);
 
+	// argument check
+	if (argc < 2) {
+        printf ("Usage: mismatch_server text_file\n");
+        return 1;
+    }
 
 	// defining a socket
 	int serv_socket_fd; // the file descriptor for server
-	int port = 56288;  //<--- you defined port here
 	if((serv_socket_fd = socket(AF_INET, SOCK_STREAM,0)) < 0){
 		perror("socket");
 		exit(1);
@@ -50,11 +49,10 @@ int main(int argc, char **argv)
 	memset(&server_addr,'\0',sizeof(server_addr)); // clear all bytes to zero
 	server_addr.sin_family = AF_INET; // set up address family NOTE: change to sin_family if this doesn't work
 	server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(port);
+    server_addr.sin_port = htons(PORT);
 
 	// bind
-	//struct sockaddr_un serv_addr; // ------------is it the same as line 32????
-	if(bind(serv_socket_fd,(struct socketadd*)&server_addr,sizeof(server_addr))){
+	if(bind(serv_socket_fd, (struct socketaddr *)&server_addr, sizeof server_addr)){
 		perror("bind");
 		return(1);
 	}
@@ -64,67 +62,94 @@ int main(int argc, char **argv)
 		perror("listen");
 		return(1);
 	} else {
-		printf("listening on port %d", port);
-	}
-
-	// accept
-	while(1){
-		struct sockaddr_in client_addr;
-		int len = sizeof (client_addr); 
-		// store the fd returned by the accept and ask fot their name
-		int client_fd; 
-		if ((client_fd = accept(serv_socket_fd, (struct sockaddr *)&client_addr, &len)) < 0) {
-			perror("accept");
-			return(1);
-		}
-		// ############# get the name of the client ######
-		printf("What is your username?");
-		scanf("%s", client_addr.username);
-
-		printf("connection from %s\n", inet_ntoa(client_addr.sin_addr));
-		addclient(client_fd, client_addr.sin_addr);
-		int tttt;
-		while (1) {  //<---- loop needs exit
-		    fd_set fdlist;
-		    FD_ZERO(&fdlist);
-		    FD_SET(client_fd, &fdlist);
-		    switch (select(serv_socket_fd+1, &fdlist, NULL, NULL, NULL)){
-				case 0: // case where client exit
-					printf("abcd\n");
-					removeclient(client_fd);
-					break;
-				case -1: // error
-					perror("select");
-					break;
-				default:  // case where we do something
-					printf("hahaha\n");
-
-					//2 cases, new connection or exist connection
-		    }	    
-		}
+		printf("listening on port %d", PORT);
 	}
 
 	// ------ add this part before the program exit 
 	// ------ so that the port will be released as soon as your server
 	// ------ process terminates
 	int on = 1;
-	int status = setsockopt([sock_fd], SOL_SOCKET, SO_REUSEADDR,
+	int status = setsockopt(serv_socket_fd, SOL_SOCKET, SO_REUSEADDR,
 		(const char *) &on, sizeof(on));
 	if(status == -1) {
 		perror("setsockopt -- REUSEADDR");
+	}
+
+	// accept
+	while(1){
+		// 
+		// ############# get the name of the client ######
+		// the max of the file descriptor, if not connected to any client,
+		// the max should be the fd of the server
+	    int maxfd = serv_socket_fd;
+	    fd_set fdlist;
+		FD_ZERO(&fdlist);	// set fdlist to zero
+		FD_SET(serv_socket_fd, &fdlist);
+		// loop through client linked list to add fd
+		for (curr = head; curr; curr = curr->next) {
+		    FD_SET(curr->fd, &fdlist);
+		    if (curr->fd > maxfd)
+				maxfd = curr->fd;
+		}
+		// select fd
+		// select will do some changes on the fdlist, and will only keep 
+		// those who is speaking to the server
+		if (select(maxfd+1, &fdlist, NULL, NULL, NULL) < 0){
+			perror("select");
+		} else {
+			for (curr = head; curr; curr = curr->next)
+				if (FD_ISSET(curr->fd, &fdlist))
+					break;
+			// if curr equals to null, this means we've loop through the linkedlist
+			// and none of the client is talking to the server
+
+			// curr exists, there is someone talking to the server	
+			if (curr){
+				// case with existing connection
+				// already accepted 
+				if (curr->state == 0) {
+					char username[MAX_NAME];
+					// return how many bytes have been read in
+					int len = read(curr->fd, username, sizeof username);
+				    if ((len > 0) && (len < 128)){
+				    	printf("sss\n");
+				    	// ###### if the user name is longer than 128 
+				    	//process_args
+				    } else if (len == 0) {
+				    	// innet_ntoa: turn an address into a string 
+						printf("Removing client %s\n", inet_ntoa(curr->ipaddr));
+						removeclient(curr->fd);
+				    } else {
+						perror("read");
+				    }
+				}
+				//
+			}
+			// if the fd of the server is in the list, means that there are new
+			// clients 
+			if (FD_ISSET(serv_socket_fd, &fdlist))
+				// case add new connection
+				newconnection(serv_socket_fd);
+				write(curr->fd, askname, strlen(askname));
+		}
+		
 	}
 	return(0);
 }
 
 void addclient(int fd, struct in_addr add){
-	struct client *p = malloc(sizeof(struct client));
+	Client *p = malloc(sizeof(Client));
 	if(!p){
 		fprintf(stderr, "fail to allocate memory\n");
 		exit(1);
 	}
 	p->fd = fd;
 	p->ipaddr = add;
-
+	p->state = 0;
+	p->inbuf = 0;
+	strcpy(p->buf, "");
+	strcpy(p->username, "");
+	p->answer = 0;
 	p->next = head;
 	head = p;
 
@@ -133,22 +158,48 @@ void addclient(int fd, struct in_addr add){
 
 
 void removeclient(int fd){
-	struct client *p = &head;
+	/*
+	Client *p = head;
 	// -----------QQQ try to find a way to reduce the running time
 	while(p != NULL){
 		p = p->next;
 		if (p->fd == fd){ 
-			struct client *temp;
+			Client *temp;
 			temp = p->next;
 			free(p);
 			p = temp;
 		}
 	}
-	printf("Removing client #######");
+	printf("Removing client #######");*/
+    struct client **p;
+    //------------- loop over the whole linked list, and set the pointer pointing to the node I wanna remove
+    for (p = &head; *p && (*p)->fd != fd; p = &(*p)->next);
+	//------------- for ( p = *top p && p->fd != fd; p = p->next)
+    if (*p) {
+		struct client *t = (*p)->next;
+			printf("Removing client %s\n", inet_ntoa((*p)->ipaddr));
+			free(*p);
+		*p = t; //------------because we have to free the memory of the node *p originally pointing to 
+    } else {
+		fprintf(stderr, "Trying to remove fd %d, but I don't know about it\n", fd);
+    }
 
 }
 
-
-
-
-
+void newconnection(int serv_socket_fd){
+	struct sockaddr_in client_addr;
+	socklen_t socklen = sizeof client_addr;
+	// store the fd returned by the accept and ask fot their name
+	int client_fd; 
+	if ((client_fd = accept(serv_socket_fd, (struct sockaddr *)&client_addr, &socklen)) < 0) {
+		perror("accept");
+	}
+	printf("connection from %s\n", inet_ntoa(client_addr.sin_addr));
+	addclient(client_fd, client_addr.sin_addr);
+}
+/*
+void getname(struct ){
+	printf("What is your username?");
+	scanf("%s", client_addr.username);
+}
+*/
